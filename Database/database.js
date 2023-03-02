fe4c0786c89156b2c0be54c748d0155fd79c1f1a
Ninthchain <73429297+Ninthchain@users.noreply.github.com>
2023-03-02 09:34:56 +0300

    first commit

---
+++ Database/database.js
@@ -0,0 +1,20 @@
+const mongodb = require('mongodb')
+
+class Database
+{
+    client
+    /**
+     * 
+     * @param {string} url 
+     */
+    constructor(url)
+    {
+        this.client = new mongodb.MongoClient(address)
+        
+    }
+
+    function start()
+    {
+
+    }
+}
